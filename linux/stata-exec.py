567f23c0c17d45fd7a162d23be76ad72a808a118
Kyle Barron <kylebarron2@gmail.com>
2018-02-23 10:57:38 -0500

    Update linux script to hopefully work on Arch Linux

--- linux/stata-exec.py
+++ linux/stata-exec.py
@@ -5,8 +5,19 @@ shortdelay = 0.2
 current = window.get_active_title()
 
 window.activate("Stata/")
-if window.wait_for_focus("Stata/(IC|SE|MP) 1[1-5].[0-2]", timeOut = 1):
 
+try:
+    if window.wait_for_focus("Stata/(IC|SE|MP) 1[1-5].[0-2]", timeOut = 1):
+
+        cmd = open(join(expanduser('~'), '.stata-exec_code'),'r').read()
+
+        time.sleep(shortdelay)
+        keyboard.send_keys(cmd)
+        keyboard.send_keys("<enter>")
+
+        time.sleep(shortdelay)
+        window.activate(current)
+except TypeError:
     cmd = open(join(expanduser('~'), '.stata-exec_code'),'r').read()
 
     time.sleep(shortdelay)
